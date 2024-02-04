16a66eb4615806da858127527696d3c19658784c
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-02-03 20:10:34 -0500

    Add: styles for mobile first approach

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let numeroSecreto = 0;
 let intentos = 0;
-// It alamacenando los números en una lista para prevenir que si ya salió no vuelva  a salir.
+// Alamacenando los números en una lista para prevenir que si ya salió no vuelva a salir.
 let listaNumerosSorteados = [];
+
+let numeroMaximo = 10;
 
 function asignarTextoElemento(elemento, texto) {
     let titulo = document.querySelector(elemento);
@@ -44,19 +46,33 @@ function limpiarCaja() {
 function generarNumeroSecreto() {
     let numeroGenerado = Math.floor(Math.random() * 10) + 1;
     // Si el numero generado esta incluido en la lista
-    if (listaNumerosSorteados) {
+    // Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        //Solucionando el error en la recursividad
+        asignarTextoElemento(
+            "p",
+            "Ya se sortearon todos los números posibles."
+        );
+    } else {
+        console.log(numeroSecreto);
+        console.log(listaNumerosSorteados);
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return;
+        }
     }
 }
 
 function condicionesIniciales() {
     asignarTextoElemento("h1", "¡Juego del número secreto!");
-    asignarTextoElemento("p", "Indica un número del 1 al 10");
+    asignarTextoElemento("p", `Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
 }
 
 function reiniciarJuego() {
-    // limpiar caja
     limpiarCaja();
     // indicar mensajes de intento de numeros
     // Genenrar el numero aleatorio
